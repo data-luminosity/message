4f5a01045816d03e9acb4ffbef3372e93e84fd83
joshjla <joshjla99@gmail.com>
2016-03-30 09:15:19 -0700

    fix python generation

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ev
 
+rm -rf query_privateanswer
+mkdir query_privateanswer
+
 ls *.thrift |
 while read line;do
-    rm -rf query_privateanswer
-    mkdir query_privateanswer
     thrift -out query_privateanswer -r --gen py ${line}
     thrift -r --gen java ${line}
     thrift -r --gen cocoa ${line}
